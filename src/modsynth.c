f30ba7bfe1d98f15a28af5bbf9330b4343d8438d
lekernel <sebastien.bourdeauducq@lekernel.net>
2009-09-03 00:45:20 +0200

    Modsynth skeleton

---
+++ src/modsynth.c
@@ -0,0 +1,33 @@
+/*
+ * Milkymist Democompo
+ * Copyright (C) 2007, 2008, 2009 Sebastien Bourdeauducq
+ * Copyright (C) 2009 Alexandre Harly
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <libc.h>
+#include <console.h>
+
+#include "modsynth.h"
+
+void modsynth_init(struct modsynth_sc *sc, void *mod_data)
+{
+	/* TODO */
+}
+
+int modsynth_synth(struct modsynth_sc *sc, short *buffer, int n)
+{
+	/* TODO */
+}
