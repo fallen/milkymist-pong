292fa5c72f21d305c42b25bbc9f0bc454eb85d55
texane <texane@debian.localdomain>
2009-09-14 18:56:22 +0000

    remove -lm dep

--- attic/modsynth-linux/src/make.sh
+++ attic/modsynth-linux/src/make.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-gcc -Wall -O2 -DUSE_MMAP -DLITTLE_ENDIAN -D_DEBUG=1 -lm main.c pcm.c mod.c file.c
+gcc -Wall -O2 -DUSE_MMAP -DLITTLE_ENDIAN -D_DEBUG=1 main.c pcm.c mod.c file.c
--- attic/modsynth-linux/src/mod.c
+++ attic/modsynth-linux/src/mod.c
@@ -19,7 +19,6 @@
  */
 
 
-#include <math.h>
 #include <stdio.h>
 #include <string.h>
 #include "mod.h"
